ff13062e8532ea0cdd92e90c79bdaa60d0b9623d
Noah Yalla <noahyalla@gmail.com>
2025-08-18 01:36:58 +1000

    mail integration

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,16 @@
-export default () => ({});
+export default ({env}) => ({
+
+    email: {
+    config: {
+      provider: 'sendgrid',
+      providerOptions: {
+        apiKey: env('SENDGRID_API_KEY'),
+        dataResidency: 'global', // or 'eu'
+      },
+      settings: {
+        defaultFrom: 'wasasoft.mobilewebdevelopment@wmwd.cloud',
+        defaultReplyTo: 'wasasoft.mobilewebdevelopment@wmwd.cloud',
+      },
+    },
+  }
+});
